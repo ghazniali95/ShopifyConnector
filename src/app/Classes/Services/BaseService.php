56ef4725be094abd6ee88844ea44df278823eed2
Samnan_jan <samnan@apimio.com>
2024-01-03 16:47:44 +0500

    pagination work is done in Product Service

---
+++ src/app/Classes/Services/BaseService.php
@@ -0,0 +1,17 @@
+<?php
+
+/***********************************************************************************************************************
+* This file is auto-generated. If you have an issue, please create a GitHub issue.                                     *
+***********************************************************************************************************************/
+
+declare(strict_types=1);
+
+namespace  Ghazniali95\ShopifyConnector\App\Classes\Services;
+
+class BaseService {
+
+   function __construct(public $channel_id) {
+        $this->channel_id = $channel_id;
+    }
+
+}
---
+++ src/app/Classes/Services/ProductService.php
@@ -0,0 +1,38 @@
+<?php
+
+/***********************************************************************************************************************
+* This file is auto-generated. If you have an issue, please create a GitHub issue.                                     *
+***********************************************************************************************************************/
+
+declare(strict_types=1);
+
+namespace  Ghazniali95\ShopifyConnector\App\Classes\Services;
+
+use Exception;
+use InvalidArgumentException;
+use Illuminate\Support\Facades\Log;
+use Ghazniali95\ShopifyConnector\App\Classes\Rest\Admin2023_10\Product;
+
+class ProductService extends BaseService
+{
+    public $product;
+    public function __construct($channel_id) {
+        $this->product = new Product();
+        $this->product->initializeSession([
+            "channel_id" => $channel_id,
+        ]);
+        parent::__construct($channel_id);
+    }
+
+    public function getAll(callable $callback) {
+        $pageInfo = null;
+        do {
+            $products = $this->product::all($this->product->session,[],['limit' => 250,'page_info' => $pageInfo]);
+            $callback($products);
+            $pageInfo = isset( $this->product::$NEXT_PAGE_QUERY['page_info']) ? $this->product::$NEXT_PAGE_QUERY['page_info'] : null;
+
+        } while ($pageInfo);
+
+    }
+
+}
